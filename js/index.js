1d31ea0ade99646eda117f65f1d0fa13de33adf0
LZL <403288582@qq.com.com>
2019-06-21 18:07:42 +0800

    lll

---
+++ js/index.js
@@ -0,0 +1,40 @@
+window.addEventListener("load",function () {
+    let main =document.querySelectorAll(".echarts");
+    let   chart = echarts.init(main[0]);
+    let line =
+        {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+    chart.setOption(line);
+    let   chart2 = echarts.init(main[1]);
+    line2 = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'line'
+        }]
+    };
+    chart2.setOption(line2);
+
+},false);
